c800e73f50c03fc67a9e4dac9515b8f50f3ea926
Opilane TTHK <opilane@tthk.ee>
2024-05-30 13:04:40 +0300

    Yes

---
+++ ForLoop/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ForLoop3MAI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 1; i <= 2; ++i)
+            {
+                Console.WriteLine("Outer: " + i); 
+               
+                for (int j = 1; j <= 3; j++)
+                {
+                    Console.WriteLine(" Inner: " + j); 
+                }
+            }
+        }
+    }
+}
---
+++ IfAndElse/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace IfAndElse3MAI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mis on sinu nimi?");
+
+            string name = Console.ReadLine();
+
+            //! e hüüumärk tähendab eitust
+            //null tähendab tühja väärtust e mitte midagi
+            if (name != null)
+            {
+                Console.WriteLine("Sisestasid enda nime:");
+                Console.WriteLine(name);
+            }
+            else
+            {
+                Console.WriteLine("ei sisestanud enda nime");
+            }
+        }
+    }
+}
